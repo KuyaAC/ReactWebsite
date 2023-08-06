d0ba91b2738a5bc7ecd6e723c5a66029adbbc8f5
KuyaAC <aciesnet02@gmail.com>
2023-08-06 22:10:29 +0800

    Halfway

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <form className='new-item-form'>
-    <label>Add New Item</label>
-    <input type='text' id='item' />
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
 
+    function handleSubmit(e){
+      e.preventDefault()
+
+      setTodos(currentTodos => {
+        return [
+          ...currentTodos,
+          {id: crypto.randomUUID(), title: newItem, completed: false},
+        ]
+      })
+
+      setNewItem("")
+    }
+
+  return (
+  <>
+  <form className='new-item-form'>
+    <div className='form-row'>
+      <label>Add New Item</label>
+      <input 
+      value={newItem}
+      onChange={e => setNewItem(e.target.value)}
+      type='text' 
+      id='item' />
+    </div>
+    <button>Add Task</button>
   </form>
+  <h1 className='header'>To Do List:</h1>
+  <ul className='list'>
+    {todos.map(todo =>{
+      return (
+      <li key={todo.id}>
+      <label>
+        <input type='checkbox' checked={todo.completed} />
+         {todo.title}
+      </label>
+      <button className='btn btn-danger'>Delete</button>
+    </li>
+      )
+    })}
+  </ul>
+  
 
-
+  </>
+  )
 }
 
 
